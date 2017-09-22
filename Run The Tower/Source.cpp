957543e6602f090fd7654f050dfdbac56b0cfc6a
Maciej Wroński <wronski_maciej@wp.pl>
2017-09-22 11:39:52 +0200

    implemented a bit of main menu

--- Run The Tower/Source.cpp
+++ Run The Tower/Source.cpp
@@ -14,11 +14,21 @@
 #define FPS 60
 
 ALLEGRO_DISPLAY *display = NULL;
-ALLEGRO_TIMER *timer = NULL;
-ALLEGRO_EVENT_QUEUE *event_queue = NULL;
+ALLEGRO_TIMER *menu_timer = NULL;
+ALLEGRO_EVENT_QUEUE *menu_event_queue = NULL;
+ALLEGRO_BITMAP *startbmp = NULL;
+ALLEGRO_BITMAP *instructionsbmp = NULL;
+ALLEGRO_BITMAP *optionsbmp = NULL;
+ALLEGRO_BITMAP *exitbmp = NULL;
+ALLEGRO_BITMAP *menusquarebmp = NULL;
+ALLEGRO_BITMAP *logobmp = NULL;
 
 float DeltaTime = 1.0 / FPS;
-bool left_game;
+bool left_menu;
+bool keys[4] = { false };
+int main_menu_choosen = 25;
+
+enum KEYS { UP, DOWN, LEFT, RIGHT };
 
 int pre_start_game() {
 	
@@ -65,24 +75,62 @@ void init_addons() {
 }
 
 void register_events_queue_timer() {
-	event_queue = al_create_event_queue();
-	timer = al_create_timer(DeltaTime);
-	al_register_event_source(event_queue, al_get_keyboard_event_source());
-	al_register_event_source(event_queue, al_get_timer_event_source(timer));
-	al_start_timer(timer);
+	menu_event_queue = al_create_event_queue();
+	menu_timer = al_create_timer(DeltaTime);
+	al_register_event_source(menu_event_queue, al_get_keyboard_event_source());
+	al_register_event_source(menu_event_queue, al_get_timer_event_source(menu_timer));
+	al_start_timer(menu_timer);
 }
 void destroy_everything() {
 	al_destroy_display(display);
-	al_destroy_timer(timer);
-	al_destroy_event_queue(event_queue);
+	al_destroy_timer(menu_timer);
+	al_destroy_event_queue(menu_event_queue);
+}
+void initialize_bitmaps() {
+	startbmp = al_load_bitmap("menu_images/start.png");
+	instructionsbmp = al_load_bitmap("menu_images/instructions.png");
+	optionsbmp = al_load_bitmap("menu_images/options.png");
+	exitbmp = al_load_bitmap("menu_images/exit.png");
+	menusquarebmp = al_load_bitmap("menu_images/choosen.png");
+	logobmp = al_load_bitmap("menu_images/logo.png");
 }
 int main(void) {
 	pre_start_game();
 	init_addons();
 	register_events_queue_timer();
-	while (!left_game) {
+	initialize_bitmaps();
+	while (!left_menu) {
+		al_clear_to_color(al_map_rgb(0, 0, 0));
+		al_draw_bitmap(logobmp, 0, 20, 0);
+		al_draw_bitmap(startbmp, 50, 300, 0);
+		al_draw_bitmap(instructionsbmp, 50, 350, 0);
+		al_draw_bitmap(optionsbmp, 50, 400, 0);
+		al_draw_bitmap(exitbmp, 50, 450, 0);
+		al_draw_bitmap(menusquarebmp, 20, 300+main_menu_choosen, 0);
 		ALLEGRO_EVENT ev;
-		al_wait_for_event(event_queue, &ev);
+		al_wait_for_event(menu_event_queue, &ev);
+		if (ev.type == ALLEGRO_EVENT_KEY_DOWN) {
+			switch (ev.keyboard.keycode) {
+			case ALLEGRO_KEY_UP: keys[UP] = true; if(main_menu_choosen - 50 > 0) main_menu_choosen -= 50;  break; // adding values to position of bmp in range of 25/175
+			case ALLEGRO_KEY_DOWN: keys[DOWN] = true; if (main_menu_choosen + 50 <= 175 ) main_menu_choosen += 50; break;
+			case ALLEGRO_KEY_RIGHT: keys[RIGHT] = true;  break;
+			case ALLEGRO_KEY_LEFT: keys[LEFT] = true;  break;
+			case ALLEGRO_KEY_ENTER:  break;
+			case ALLEGRO_KEY_ESCAPE: break;
+			}
+		}
+		else if (ev.type == ALLEGRO_EVENT_KEY_UP)
+		{
+			switch (ev.keyboard.keycode) {
+			case ALLEGRO_KEY_UP: keys[UP] = false;  break;
+			case ALLEGRO_KEY_DOWN: keys[DOWN] = false;  break;
+			case ALLEGRO_KEY_RIGHT: keys[RIGHT] = false;  break;
+			case ALLEGRO_KEY_LEFT: keys[LEFT] = false;  break;
+			case ALLEGRO_KEY_ENTER:break;
+			case ALLEGRO_KEY_ESCAPE: break;
+			}
+		}
+		al_flip_display();
 	}
 	destroy_everything();
 	return 0;
